1657d959b6eac4bcdddbea876928afff209a231f
Kevin Johr <kevin.johr@posteo.de>
2023-12-15 23:36:22 +0100

    #1 add first BotSettings

---
+++ BotSettings/BotConfig.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Balu_Ass_2.BotSettings
+{
+    internal class BotConfig
+    {
+        public _BotConnectionSettings BotSettings { get; set; }
+        public _ChannelIds ChannelIds { get; set; }
+        public _DatabaseConnectionString DatabaseConnectionString { get; set; }
+    }
+}
