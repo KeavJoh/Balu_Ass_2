d38c73ea3701972d0261dc0373887ada46204178
Kevin Johr <kevin.johr@posteo.de>
2023-12-15 23:14:09 +0100

    #1 prepare class program

--- Program.cs
+++ Program.cs
@@ -1,4 +1,9 @@
-﻿class Programm
-{
+﻿using DSharpPlus;
+using DSharpPlus.CommandsNext;
+using System.Security;
 
+class Programm
+{
+    private static DiscordClient Client { get; set; }
+    private static CommandsNextExtension Commands { get; set; }
 }
